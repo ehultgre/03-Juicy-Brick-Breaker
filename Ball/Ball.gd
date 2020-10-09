1ae25a9998a7bfa7e041785f16f1190f2a3e2fc9
ehultgre <70243373+ehultgre@users.noreply.github.com>
2020-10-09 00:05:21 -0400

    Ball/Paddle

---
+++ Ball/Ball.gd
@@ -0,0 +1,41 @@
+extends RigidBody2D
+
+onready var VP = get_viewport_rect().size
+export var min_speed = 100.0
+export var max_speed = 400.0
+
+func _ready():
+	contact_monitor = true
+	set_max_contacts_reported(4)
+	position = Vector2(VP.x/2, VP.y - 200)
+
+
+func _physics_process(_delta):
+	if position.y > VP.y + 30:
+		die()
+	
+	
+	var bodies = get_colliding_bodies()
+	for body in bodies:
+		if body.get_parent().name =="Paddle Container":
+			$AnimatedSprite.play("hit")
+			body.find_node("AnimatedSprite").play("hit")
+
+
+func _integrate_forces(state):
+	if abs(state.linear_velocity.x) < min_speed:
+		state.linear_velocity.x = sign(state.linear_velocity.x) * min_speed
+	if abs(state.linear_velocity.x) > max_speed:
+		state.linear_velocity.x = sign(state.linear_velocity.x) * max_speed
+	if abs(state.linear_velocity.y) < min_speed:
+		state.linear_velocity.y = sign(state.linear_velocity.y) * min_speed
+	if abs(state.linear_velocity.y) > max_speed:
+		state.linear_velocity.y = sign(state.linear_velocity.y) * max_speed
+
+
+func die():
+	queue_free()
+
+
+func _on_AmimatedSprite_animation_finished():
+	$AnimatedSprite.play("default")
---
+++ Ball/Ball_Container.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+onready var Ball = load("res://Ball/Ball.tscn")
+var speed = 300
+
+func _ready():
+	randomize()
+
+func _physics_process(_delta):
+	if get_child_count() == 0:
+		load_ball()
+
+func load_ball():
+	var ball = Ball.instance()
+	var impulse = Vector2.RIGHT*speed
+	impulse = impulse.rotated(randf()*-PI)
+	ball.apply_central_impulse(impulse)
+	add_child(ball)
